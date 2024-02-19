963b89bcc1da1ee3ada7dc29af2ee9d9d118c921
sergio-slima <sergio_silvalima@hotmail.com>
2024-02-19 06:37:44 -0300

    config tela splash

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+import { styles } from './styles';
 
 export function SignIn() {
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
       <Text>Hello World, Sergio!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput 
+        style={styles.input} 
+        onChangeText={setText}
+      />
+
+      <Text>Digitado: {text}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
