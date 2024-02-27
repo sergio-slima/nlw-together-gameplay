4d3824ec9953ad0450041d47b81f1fcb0cf09669
sergio-slima <sergio_silvalima@hotmail.com>
2024-02-26 21:15:13 -0300

    create profile home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+import { styles } from "./styles";
+
+import { Profile } from "../../components/Profile";
+
+export function Home(){
+    return(
+        <View style={styles.header}>
+            <Profile />
+        </View>
+    );
+}
